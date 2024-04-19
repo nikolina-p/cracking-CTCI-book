6ccc01a5bdf627babf01953b67006ff0dadbbe63
nikolina-p <nikolinap85@gmail.com>
2024-04-19 15:53:13 -0700

    Q4_10 - check subtree. Fixes the bug: when nodes do not match , start checking from the root of T2 (shown in TEST 2, that was returning True previously).

--- Q4_10_check_subtree.py
+++ Q4_10_check_subtree.py
@@ -21,7 +21,11 @@ class BinaryTree:
     def __init__(self, root: Node = None):
         self.root = root
 
-    def is_subtree(self, t1: Node, t2: Node) -> bool:
+    def is_subtree(self, t1: Node, t2: Node, r=None) -> bool:
+        if r is None:
+            # root of T2, if nodes do not match, we have to start over, checking from the root of T2 (TEST 2)
+            r = t2
+
         if t1 is None and t2 is None:
             return True
 
@@ -29,9 +33,9 @@ class BinaryTree:
             return False
 
         if t1.value == t2.value:
-            return self.is_subtree(t1.left, t2.left) and self.is_subtree(t1.right, t2.right)
+            return self.is_subtree(t1.left, t2.left, r) and self.is_subtree(t1.right, t2.right, r)
         else:
-            return self.is_subtree(t1.left, t2) or self.is_subtree(t1.right, t2)
+            return self.is_subtree(t1.left, r, r) or self.is_subtree(t1.right, r, r)
 
 
 if __name__ == '__main__':
@@ -53,10 +57,11 @@ if __name__ == '__main__':
     d.right = f
     e.left = a
     e.right = g
-    a.left = b
+    a.left = Node("x")
+    a.right = Node("y")
     f.left = t1a
     t1a.left = t1b
-    t1a.right = t1c
+    #t1a.right = t1c
 
     tree_print(t1.root)
 
@@ -66,21 +71,69 @@ if __name__ == '__main__':
 
     tree_print(t2.root)
 
-    print(f"IS subtree: {t1.is_subtree(t1.root, t2.root)}")
+    print(f"IS subtree (TEST 1): {t1.is_subtree(t1.root, t2.root)}")
 
-"""
+    n1 = Node("A")
+    n2 = Node("X")
+    n3 = Node("B")
+    n4 = Node("B")
+    n5 = Node("D")
+    n6 = Node("E")
+    n7 = Node("D")
+    n8 = Node("E")
+
+    n1.left = n2
+    n2.left = n3
+    n2.right = n4
+    n3.left = n5
+    n3.right = n6
+    n4.left = n7
+    n4.right = n8
+
+    s1 = Node("A")
+    s2 = Node("B")
+    s3 = Node("D")
+    s4 = Node("E")
+
+    s1.left = s2
+    s2.left = s3
+    s2.right = s4
+
+    s_tree = BinaryTree(s1)
+    n_tree = BinaryTree(n1)
+
+    tree_print(n1)
+    tree_print(s1)
+
+    print(f"IS subtree (TEST 2): {n_tree.is_subtree(n_tree.root, s_tree.root)}")
+
+
+r"""
 OUTPUT:
 
-                         d                         
-            /                        \            
-            e                        f            
-      /            \            /            \      
-      *            *            a            *      
-   /      \      /      \      /      \      /      \   
-   *      *      *      *      b      c      *      *   
+                        d                       
+           /                      \           
+           e                      f           
+     /          \          /          \     
+     a          g          a          *     
+  /    \    /    \    /    \    /    \  
+  x    y    *    *    b    *    *    *  
     a    
   /    \  
   b    c  
-IS subtree: True
+IS subtree (TEST 1): False
+                      A                      
+           /                      \           
+           X                      *           
+     /          \          /          \     
+     B          B          *          *     
+  /    \    /    \    /    \    /    \  
+  D    E    D    E    *    *    *    *  
+           A           
+     /          \     
+     B          *     
+  /    \    /    \  
+  D    E    *    *  
+IS subtree (TEST 2): False
 
 """
